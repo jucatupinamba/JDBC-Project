e6f9735780f0fa012d48a9a1db8bb238b609b2fe
Juarez Martins <juniorbmx13@gmail.com>
2023-05-31 13:17:23 -0300

    feat: deletar dados, tratamento de exceções de integridade referencial (proteger a chave pai se conter outras chaves filhas vinculadas para evitar perda de dados)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import db.DB;
+import db.DbIntegrityException;
 
 import java.sql.*;
 import java.text.ParseException;
@@ -16,20 +17,18 @@ public class Program {
             conn = DB.getConnection();
 
             st = conn.prepareStatement(
-                    "UPDATE seller "
-                            + "SET BaseSalary = BaseSalary + ? "
-                            + "WHERE "
-                            + "(DepartmentId = ?)");
+                    "DELETE FROM department "
+                    + "WHERE "
+                    + "Id = ?");
 
-            st.setDouble(1, 200.0);
-            st.setInt(2, 2);
+            st.setInt(1, 2);
 
             int rowsAffected = st.executeUpdate();
 
             System.out.println("Done! Rows affected: " + rowsAffected);
         }
         catch (SQLException e){
-            e.printStackTrace();
+            throw new DbIntegrityException(e.getMessage());
         }
         finally {
             DB.closeStatement(st);
